a6df9b1f30fd4f7f95c40a18ff3b65c3b4f95ee3
Huix <firelee074@gmail.com>
2023-08-17 21:43:11 +0800

    DynamicTexture

--- mite-shader-loader/src/net/wenscHuix/mitemod/shader/mixin/render/texture/DynamicTextureMixin.java
+++ mite-shader-loader/src/net/wenscHuix/mitemod/shader/mixin/render/texture/DynamicTextureMixin.java
@@ -12,6 +12,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(bib.class)
@@ -30,22 +31,28 @@ public class DynamicTextureMixin extends bia {
     public void a(bjp bjp) {
     }
 
-//    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/bip;a(III)V"), method = "<init>(II)V")
-//    private void redirectInit(int id, int par1, int par2){
-//        if(Shaders.isActiveShader) {
-//            ShadersTex.initDynamicTexture(id, par1, par2, ReflectHelper.dyCast(this));
-//        } else {
-//            bip.a(id, par1, par2);
-//        }
-//    }
-//
-//    @Overwrite
-//    public void a() {
-//        if(Shaders.isActiveShader) {
-//            ShadersTex.updateDynamicTexture(this.b(), this.b, this.c, this.d, ReflectHelper.dyCast(this));
-//        } else {
-//            bip.a(this.b(), this.b, this.c, this.d);
-//        }
-//    }
+    @ModifyVariable(at = @At(value = "FIELD" , shift = At.Shift.AFTER, ordinal = 1), method = "<init>(II)V", argsOnly = true, index = 1)
+    private int injectInit(int par1){
+        return par1 * 3;
+    }
+
+
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/bip;a(III)V"), method = "<init>(II)V")
+    private void redirectInit(int id, int par1, int par2){
+        if(Shaders.isActiveShader) {
+            ShadersTex.initDynamicTexture(id, par1 / 3, par2, ReflectHelper.dyCast(this));
+        } else {
+            bip.a(id, par1 / 3, par2);
+        }
+    }
+
+    @Overwrite
+    public void a() {
+        if(Shaders.isActiveShader) {
+            ShadersTex.updateDynamicTexture(this.b(), this.b, this.c, this.d, ReflectHelper.dyCast(this));
+        } else {
+            bip.a(this.b(), this.b, this.c, this.d);
+        }
+    }
 
 }
