bf6958371a4792a7f462e157ffa95fa697704ee4
Huix <firelee074@gmail.com>
2023-08-17 13:57:57 +0800

    Mixin OK

--- mite-shader-loader/src/net/wenscHuix/mitemod/shader/mixin/render/TextureAtlasSpriteMixin.java
+++ mite-shader-loader/src/net/wenscHuix/mitemod/shader/mixin/render/TextureAtlasSpriteMixin.java
@@ -1,25 +1,116 @@
 package net.wenscHuix.mitemod.shader.mixin.render;
 
 
-import net.minecraft.bil;
-import net.minecraft.bip;
-import net.minecraft.bjp;
+import com.google.common.collect.Lists;
+import net.minecraft.*;
 import net.wenscHuix.mitemod.shader.client.Shaders;
 import net.wenscHuix.mitemod.shader.client.ShadersTex;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.spongepowered.asm.mixin.injection.modify.LocalVariableDiscriminator;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @Mixin(bil.class)
 public class TextureAtlasSpriteMixin {
 
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/bip;a([IIIIIZZ)V"), method = "j")
+    private void redirectRenderLivingLabel0(int[] var0, int var1, int var2, int var3, int var4, boolean var5, boolean var6) {
+        if(Shaders.isActiveShader) {
+            ShadersTex.updateSubImage(((int[])this.a.get(var3)), this.e, this.f, this.c, this.d, false, false);
+        } else {
+            bip.a(((int[])this.a.get(var3)), this.e, this.f, this.c, this.d, false, false);
+        }
+    }
+
+//    @Inject(locals = LocalCapture.CAPTURE_FAILHARD, at = @At(value = "INVOKE", target = "Ljava/awt/image/BufferedImage;getRGB(IIII[III)[I", shift = At.Shift.BEFORE),
+//            method = "a(Lnet/minecraft/bjn;)V")
+//    private void injectLoadSprite0(CallbackInfo callbackInfo, BufferedImage var4, int[] var5){
+//        if(Shaders.isActiveShader) {
+//            ShadersTex.loadAtlasSprite(var4, 0, 0,this.e, this.f, var5, 0, this.e);
+//        }
+//    }
+
+    @Overwrite
+    public final void a(bjn par1Resource) throws IOException {
+        this.n();
+        InputStream var2 = par1Resource.b();
+        bko var3 = (bko)par1Resource.a("animation");
+        BufferedImage var4 = ImageIO.read(var2);
+        this.f = var4.getHeight();
+        this.e = var4.getWidth();
+        int[] var5 = new int[this.f * this.e * 3];
+        if(Shaders.isActiveShader) {
+            ShadersTex.loadAtlasSprite(var4, 0, 0,  this.e, this.f, var5, 0, this.e);
+        }
+        var4.getRGB(0, 0, this.e, this.f, var5, 0, this.e);
+        if (var3 == null) {
+            if (this.f != this.e) {
+                throw new RuntimeException("broken aspect ratio and not an animation");
+            }
+
+            this.a.add(var5);
+        } else {
+            int var6 = this.f / this.e;
+            int var7 = this.e;
+            int var8 = this.e;
+            this.f = this.e;
+            int var10;
+            if (var3.c() > 0) {
+
+                for (Object o : var3.e()) {
+                    var10 = (Integer) o;
+                    if (var10 >= var6) {
+                        throw new RuntimeException("invalid frameindex " + var10);
+                    }
+
+                    this.d(var10);
+                    if(Shaders.isActiveShader) {
+                        this.a.set(var10, ShadersTex.extractFrame(var5, var7, var8, var10));
+                    } else{
+                        this.a.set(var10, a(var5, var7, var8, var10));
+                    }
+                }
+
+                this.j = var3;
+            } else {
+                ArrayList var11 = Lists.newArrayList();
+
+                for(var10 = 0; var10 < var6; ++var10) {
+                    if(Shaders.isActiveShader) {
+                        this.a.add(ShadersTex.extractFrame(var5, var7, var8, var10));
+                    } else {
+                        this.a.add(a(var5, var7, var8, var10));
+                    }
+                    var11.add(new bkn(var10, -1));
+                }
+
+                this.j = new bko(var11, this.e, this.f, var3.d());
+            }
+        }
+    }
+
+    public boolean load(bjp manager, bjo location) throws IOException {
+        this.a(ShadersTex.loadResource(manager, location));
+        return true;
+    }
+
+
+    @Shadow
+    private bko j;
     @Shadow
     protected List a;
     @Shadow
@@ -32,23 +123,20 @@ public class TextureAtlasSpriteMixin {
     protected int e;
     @Shadow
     protected int f;
+    @Shadow
+    @Final
+    private void n() {
 
-    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/bip;a([IIIIIZZ)V"), method = "j")
-    private void redirectRenderLivingLabel0(int[] var0, int var1, int var2, int var3, int var4, boolean var5, boolean var6) {
-        if(Shaders.isActiveShader) {
-            ShadersTex.updateSubImage(((int[])this.a.get(var3)), this.e, this.f, this.c, this.d, false, false);
-        } else {
-            bip.a(((int[])this.a.get(var3)), this.e, this.f, this.c, this.d, false, false);
-        }
     }
+    @Shadow
+    @Final
+    private void d(int par1) {
 
-    @Inject(locals = LocalCapture.PRINT, at = @At(value = "INVOKE", target = "Ljava/awt/image/BufferedImage;getRGB(IIII[III)[I", shift = At.Shift.BEFORE),
-            method = "a(Lnet/minecraft/bjn;)V")
-    private void injectLoadSprite0(CallbackInfo callbackInfo){
-        if(Shaders.isActiveShader) {
-            ShadersTex.loadAtlasSprite(var4, 0, 0, this.width, this.height, var5, 0, this.width);
-        }
     }
-
+    @Shadow
+    @Final
+    private static int[] a(int[] par0ArrayOfInteger, int par1, int par2, int par3) {
+        return null;
+    }
 
 }
---
+++ mite-shader-loader/src/net/wenscHuix/mitemod/shader/mixin/render/TextureManagerMixin.java
@@ -0,0 +1,48 @@
+package net.wenscHuix.mitemod.shader.mixin.render;
+
+
+import net.minecraft.*;
+import net.wenscHuix.mitemod.shader.client.Shaders;
+import net.wenscHuix.mitemod.shader.client.ShadersTex;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.Map;
+
+@Mixin(bim.class)
+public class TextureManagerMixin {
+
+//    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/bio;b()I"), method = "a(Lnet/minecraft/bjo;)V")
+//    private void redirectUpdateCompass(int var0) {
+//        if(Shaders.isActiveShader) {
+//            ShadersTex.bindTexture((TextureObject)var2);
+//        } else {
+//            bip.b(var0);
+//        }
+//    }
+    @Overwrite
+    public void a(bjo par1ResourceLocation) {
+        Object var2 = this.a.get(par1ResourceLocation);
+        if (var2 == null) {
+            var2 = new bif(par1ResourceLocation);
+            this.a(par1ResourceLocation, (bio) var2);
+        }
+        if (Shaders.isActiveShader) {
+            ShadersTex.bindTexture((bio) var2);
+        } else {
+            bip.b(((bio) var2).b());
+        }
+    }
+
+    @Shadow
+    @Final
+    private Map a;
+    @Shadow
+    public boolean a(bjo par1ResourceLocation, bio par2TextureObject) {
+        return false;
+    }
+}
